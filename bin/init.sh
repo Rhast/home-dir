e5376657975c0ab38e3642ef47554fc6a2358b61
ler-w <lermontov.w@gmail.com>
2025-01-12 12:18:00 +0900

    improved setup

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,29 @@
-#!/bin/bash
-echo 'Installing Homebrew...'
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval $(/opt/homebrew/bin/brew shellenv)
+check_and_install() {
+    local cmd=$1
+    shift
+    local install_cmd=("$@")
 
-echo 'Installing git credentials helper ...'
-brew install --cask git-credential-manager
+    if ! command -v $cmd &> /dev/null
+    then
+        echo "Installing $cmd..."
+        "${install_cmd[@]}"
+    else
+        echo "$cmd is already installed"
+    fi
+}
+
+# Example usage of the function
+check_and_install brew \
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval $(/opt/homebrew/bin/brew shellenv)
+
+check_and_install git-credential-manager \
+    brew install --cask git-credential-manager
+
+check_and_install zed \
+    brew install --cask zed
+
+check_and_install ghostty \
+    brew install --cask ghostty
+
+echo 'Configuring Mac Os shortcuts...'
+defaults write com.apple.finder NSUserKeyEquivalents -dict-add "Go to Folder..." -string "@L"
