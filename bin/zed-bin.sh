4755a0f8467eee421cc67a4fa6e9c1a9350a71d6
ler-w <lermontov.w@gmail.com>
2025-02-08 12:17:33 +0900

    sync with main

---
+++ bin/zed-bin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title zed-bin
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon /Applications/Zed.app/Contents/Resources/Zed.icns
+
+zed -n "$HOME"//bin
